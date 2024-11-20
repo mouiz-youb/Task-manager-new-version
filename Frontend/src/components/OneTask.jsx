52eb2084f2f68465e73435fb8b06eadcc8096f6e
abdelmouiz <a_youb@estin.dz>
2024-11-20 08:42:21 -1100

    create the tasks compenent like the three task

---
+++ Frontend/src/components/OneTask.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../App.css";
+function OneTask() {
+  return (
+    <div className="one-task">
+      <div className="Task-title">
+        <h1>Task title</h1>
+      </div>
+      <div className="task-descreption">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
+        explicabo magni vitae culpa praesentium mollitia totam placeat,
+        consequuntur vero accusantium asperiores deleniti nulla voluptatum
+        cupiditate rem velit? Eveniet, adipisci dignissimos. Lorem ipsum, dolor
+        sit amet consectetur adipisicing elit. Maxime explicabo magni vitae
+        culpa praesentium mollitia totam placeat, consequuntur vero accusantium
+        asperiores deleniti nulla voluptatum cupiditate rem velit? Eveniet,
+        adipisci dignissimos. Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Maxime explicabo magni vitae culpa praesentium
+        mollitia totam placeat, consequuntur vero accusantium asperiores
+        deleniti nulla voluptatum cupiditate rem velit? Eveniet, adipisci
+        dignissimos. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Maxime explicabo magni vitae culpa praesentium mollitia totam placeat,
+        consequuntur vero accusantium asperiores deleniti nulla voluptatum
+        cupiditate rem velit? Eveniet, adipisci dignissimos. Lorem ipsum, dolor
+        sit amet consectetur adipisicing elit. Maxime explicabo magni vitae
+        culpa praesentium mollitia totam placeat, consequuntur vero accusantium
+        asperiores deleniti nulla voluptatum cupiditate rem velit? Eveniet,
+        adipisci dignissimos. Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Maxime explicabo magni vitae culpa praesentium
+        mollitia totam placeat, consequuntur vero accusantium asperiores
+        deleniti nulla voluptatum cupiditate rem velit? Eveniet, adipisci
+        dignissimos.
+      </div>
+      <div className="task-time">
+        <div className="time-section">time</div>
+        <div className="btn-section">
+          <button>delete</button>
+          <button>update</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OneTask;
--- Frontend/src/components/TaskThree.jsx
+++ Frontend/src/components/TaskThree.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import "../App.css";
+import OneTask from "./OneTask";
+import Nav from "./Nav";
+
 function TaskThree() {
-  return <div>TaskThree</div>;
+  return (
+    <div className="Task-container-three">
+      <div className="header-container">
+        <h1>Last three task</h1>
+      </div>
+      <div className="Task-nav-links">
+        <Nav />
+      </div>
+      <div className="Last-three-task">
+        <OneTask />
+        <OneTask />
+        <OneTask />
+      </div>
+    </div>
+  );
 }
 
 export default TaskThree;
