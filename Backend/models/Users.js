d1227872bdd134b2d2848f26ca1b22a041a0c7ca
abdelmouiz <a_youb@estin.dz>
2024-11-21 06:28:43 -1100

    deleting the first backend folder

--- Backend/models/Users.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const Schema = mongoose.Schema;
-const UserSchema = new Schema({
-  username: String,
-  email: String,
-  password: String,
-});
-const User = mongoose.model("User", UserSchema);
-export default User;
