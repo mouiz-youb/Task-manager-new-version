c5d042d52e6d6b44370e8d7630315f8029f37564
abdelmouiz <a_youb@estin.dz>
2024-11-26 02:40:41 -1100

    now i fixe the problem of the scroll the next step i need to do the scroll effect using framer motion

--- Frontend2/src/components/Home.jsx
+++ Frontend2/src/components/Home.jsx
@@ -7,13 +7,73 @@ function Home() {
     <div className="Home-container">
       <h1 className="home-title">Welcome to my website</h1>
       <div className="home-content">
-        <div className="lines-home"></div>
-        <div className="lines-home"></div>
-        <div className="lines-home"></div>
-        <div className="lines-home"></div>
+        <div className="lines-home">
+          <motion.div
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.9 }}
+            className="left-side"
+          ></motion.div>
+          <motion.div
+            variants={fadeIn("up", 1)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.9 }}
+            className="right-side"
+          ></motion.div>
+        </div>
+        <div className="lines-home">
+          <motion.div
+            variants={fadeIn("up", 1)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.9 }}
+            className="left-side"
+          ></motion.div>
+          <motion.div
+            variants={fadeIn("up", 1)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.9 }}
+            className="right-side"
+          ></motion.div>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Home;
+
+{
+  /* <div className="lines-home">
+        <div className="lines-home">
+          <motion.div
+            variants={fadeIn("left", 1)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.9 }}
+            className="left-side"
+          ></motion.div>
+          <motion.div
+            variants={fadeIn("right", 1)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.9 }}
+            className="right-side"
+          ></motion.div>
+        </div>
+        <div className="lines-home">
+  <div className="left-side"></div>
+  <div className="right-side"></div>
+</div>
+<div className="lines-home">
+  <div className="left-side"></div>
+  <div className="right-side"></div>
+</div>
+<div className="lines-home">
+  <div className="left-side"></div>
+  <div className="right-side"></div>
+</div> */
+}
